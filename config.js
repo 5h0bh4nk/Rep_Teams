87219606464112819bcd8da992b049f63cbdfe18
Shubh_ank <62099471+5h0bh4nk@users.noreply.github.com>
2021-07-12 14:24:50 +0530

    config added

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
     'secretKey':'YOUR_SECRET_KEY',
     // change mongo Url below to your DB
     // NOTE: hosted version uses different database and this is test database
-    'mongoUrl': 'mongodb+srv://shubh4nk:<password>@cluster0.s7x0c.mongodb.net/production',
+    'mongoUrl': 'mongodb+srv://shubh4nk:testpass@cluster0.uws21.mongodb.net/test',
     'facebook': {
         clientId: 'YOUR_CLIENT_ID',
         clientSecret: 'YOUR_CLIENT_SECRET'
