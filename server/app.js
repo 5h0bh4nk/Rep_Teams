9d4caddfd23736f076c514516e509f46c4429cd7
Shubh4nk <kul.shubhank207@gmail.com>
2025-05-24 17:39:52 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -59,7 +59,7 @@ app.use(cors({
 		"http://localhost:3000", 
 		"http://192.168.1.9:8000", 
 		"http://192.168.1.9:3000",
-		FRONTEND_URL,
+		"https://shubhankteams.netlify.app",
 		process.env.FRONTEND_URL
 	].filter(Boolean),
 	methods: ["GET", "POST", "PUT", "DELETE"],
