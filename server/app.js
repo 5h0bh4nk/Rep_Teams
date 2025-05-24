08f0c9ef959e61a8e52741381d72946a302f9bbb
Shubh4nk <kul.shubhank207@gmail.com>
2025-05-24 17:26:05 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -34,6 +34,7 @@ var io = require('socket.io')(server, {
 			"http://localhost:3000", 
 			"http://192.168.1.9:8000", 
 			"http://192.168.1.9:3000",
+			"https://shubhankteams.netlify.app",
 			process.env.FRONTEND_URL
 		].filter(Boolean),
 		methods: ["GET", "POST"],
